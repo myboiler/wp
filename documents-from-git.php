7c131be0a7e0d906feb479c0ef376f50b07c755c
myboiler-admin <77276197+myboiler-admin@users.noreply.github.com>
2023-07-12 08:18:00 +0100

    Update documents-from-git.php

--- documents-from-git.php
+++ documents-from-git.php
@@ -2,7 +2,7 @@
 				<?php 
 				$slug = get_post_field( 'post_name');
 				echo '<div class="github" style="padding-top:100px">';
-				echo do_shortcode('[git-github-markdown url="https://github.com/myboiler/blob/main/content/'.$slug.'.md" cache_ttl="60" cache_strategy=static]');
+				echo do_shortcode('[git-github-markdown url="https://github.com/myboiler/wp/blob/main/content/'.$slug.'.md" cache_ttl="60" cache_strategy=static]');
 				echo '</div>';
 				?>
 				<!-- end of GH MD code -->
