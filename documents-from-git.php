963a89999feab81990e926c926118bcbfc3fdeb0
myboiler-admin <77276197+myboiler-admin@users.noreply.github.com>
2023-07-12 08:10:27 +0100

    Create documents-from-git.php

---
+++ documents-from-git.php
@@ -0,0 +1,8 @@
+				<!-- GH MD code, 12/07/23, JK-->
+				<?php 
+				$slug = get_post_field( 'post_name');
+				echo '<div class="github" style="padding-top:100px">';
+				echo do_shortcode('[git-github-markdown url="https://github.com/myboiler/blob/main/content/'.$slug.'.md" cache_ttl="60" cache_strategy=static]');
+				echo '</div>';
+				?>
+				<!-- end of GH MD code -->
